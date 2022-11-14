d3f9e287549b6a2787b1de0e5afb30d50ce0355a
kristina-lim <kristinahlim@gmail.com>
2022-11-14 15:38:26 -0800

    Add more elements to dogs array

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 dogs = [
   {'name': 'Chopin', 'breed': 'yorkshire terrier', 'description': 'cute, hairy, little, spoiled demon', 'age': 8},
   {'name': 'Duke', 'breed': 'border collie', 'description': 'energetic furball', 'age': 12},
+  {'name': 'Lily', 'breed': 'chihuahua dotson', 'description': 'sweet but rowdy', 'age': 2},
+  {'name': 'Po', 'breed': 'chihuahua dotson', 'description': 'shy and gentle', 'age': 2},
+  {'name': 'Louie', 'breed': 'chihuahua dotson', 'description': 'old man vibes but very playful', 'age': 3},
+  {'name': 'Monty', 'breed': 'chihuahua', 'description': 'looks crazy cause he is crazy', 'age': 3},
 ]
 
 # Create your views here.
