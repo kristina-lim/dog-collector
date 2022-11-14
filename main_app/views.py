977527fe96ecc6cf44e3539d76c10a00d867d90b
kristina-lim <kristinahlim@gmail.com>
2022-11-14 14:27:58 -0800

    CSS and Finish Part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+dogs = [
+  {'name': 'Chopin', 'breed': 'yorkshire terrier', 'description': 'cute, hairy, little, spoiled demon', 'age': 8},
+  {'name': 'Duke', 'breed': 'border collie', 'description': 'energetic furball', 'age': 12},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', {
+        'dogs': dogs
+    })
