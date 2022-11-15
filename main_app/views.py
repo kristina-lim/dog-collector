fc122d48ef12031a47fa71766e7d4358a73f475a
kristina-lim <kristinahlim@gmail.com>
2022-11-15 11:42:01 -0800

    register dog model with admin portal and make details page

--- main_app/views.py
+++ main_app/views.py
@@ -9,6 +9,13 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {
         'dogs': dogs
+    })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', {
+        'dog': dog
     })
