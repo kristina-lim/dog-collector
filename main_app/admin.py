e76e8be0c0aa1294f98ed67b84d9163a7f5d5568
kristina-lim <kristinahlim@gmail.com>
2022-11-16 15:12:33 -0800

    Add another model and see a list of feedings

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Dog
+from .models import Dog, Feeding
 
 # Register your models here.
 admin.site.register(Dog)
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 # Create your models here.
 class Dog(models.Model):
   name = models.CharField(max_length=100)
@@ -13,3 +19,16 @@ class Dog(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'dog_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+    )
+  
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.get_meal_display()} on {self.date}'
